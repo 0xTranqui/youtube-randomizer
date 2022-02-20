c4fdeaebf8360bbe319ebc55ea37d0f0db4bbffa
tranqui.eth <thinkandchill11@gmail.com>
2022-02-20 20:06:25 -0300

    Uploading files

---
+++ scripts.js
@@ -0,0 +1,34 @@
+let defaultVid = "https://www.youtube.com/embed/uxgHmbM3Ig0";
+
+document.getElementById("iframe").src = defaultVid;   
+
+const artVids = ["https://www.youtube.com/embed/IqVXThss1z4", "https://www.youtube.com/embed/-YPOWBQAd1M", "https://www.youtube.com/embed/q2bwZ-nUxhU"];
+
+const sportsVids = ["https://www.youtube.com/embed/1lvkeUjEMlA", "https://www.youtube.com/embed/OTTWedyp37o", "https://www.youtube.com/embed/o1diW_2W-cM"];
+
+const web3Vids = ["https://www.youtube.com/embed/b2z-UCXAJRA", "https://www.youtube.com/embed/dwjiucS4NtY", "https://www.youtube.com/embed/eim8REOYLzA"];
+
+
+function vidPicker() {
+    let viewerVidSelection = document.getElementById("selection").selectedIndex;
+    if (viewerVidSelection === 0) {
+        document.getElementById("iframe").src = defaultVid;
+    }
+    else if (viewerVidSelection === 1) {
+        let artId = Math.floor((Math.random()*(artVids.length)));
+        document.getElementById("iframe").src = artVids[artId];
+    }
+    else if (viewerVidSelection === 2) {
+        let sportsId = Math.floor((Math.random()*(sportsVids.length)));
+        document.getElementById("iframe").src = sportsVids[sportsId];
+    }
+    else {
+        let web3Id = Math.floor((Math.random()*(web3Vids.length)));
+        document.getElementById("iframe").src = web3Vids[web3Id];
+    }
+}
+
+document.getElementById("randomizer").addEventListener("click", vidPicker());
+
+
+
