1ce54a2e4d6b178020478bd4ed282f1f71238c69
tranqui.eth <thinkandchill11@gmail.com>
2022-02-20 20:18:58 -0300

    updated videos

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ document.getElementById("iframe").src = defaultVid;
 
 const artVids = ["https://www.youtube.com/embed/IqVXThss1z4", "https://www.youtube.com/embed/-YPOWBQAd1M", "https://www.youtube.com/embed/q2bwZ-nUxhU"];
 
-const sportsVids = ["https://www.youtube.com/embed/1lvkeUjEMlA", "https://www.youtube.com/embed/OTTWedyp37o", "https://www.youtube.com/embed/o1diW_2W-cM"];
+const sportsVids = ["https://www.youtube.com/embed/1lvkeUjEMlA", "https://www.youtube.com/embed/OTTWedyp37o", "https://www.youtube.com/embed/o1diW_2W-cM","https://www.youtube.com/embed/qT5GlgXrX-0", "https://www.youtube.com/embed/yMQyQRw1Bbc"];
 
-const web3Vids = ["https://www.youtube.com/embed/b2z-UCXAJRA", "https://www.youtube.com/embed/dwjiucS4NtY", "https://www.youtube.com/embed/eim8REOYLzA"];
+const web3Vids = ["https://www.youtube.com/embed/b2z-UCXAJRA", "https://www.youtube.com/embed/dwjiucS4NtY", "https://www.youtube.com/embed/eim8REOYLzA", "https://www.youtube.com/embed/7Kq3YWsysc0", "https://www.youtube.com/embed/b1m_PTVxD-s"];
 
 
 function vidPicker() {
